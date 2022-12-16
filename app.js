5d8a233f2276087d034b7a49c7081223b0986b14
marvin <marvinwolfart2@gmail.com>
2022-12-16 16:24:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import fetch from 'node-fetch'
 const app = express();
 
 const port = 7777;
@@ -7,24 +6,7 @@ const port = 7777;
 
 app.get("/", (req, res) => {
   console.log("NEW REQUEST!");
-  fetch("https://maker.ifttt.com/trigger/notif/with/key/dav-twDkW2HET8-YMRLvX2764V-U9q7KDS_Mj9SkXAC", {
-    method: "POST",
-    headers: {
-      "Content-Type" : "application/json"
-    },
-    body: JSON.stringify({
-      "value1":"New Custom Notification",
-      "value2":"testetstestetestst",
-      "value3":"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-    })
-  })
-  .then((res) => {
-    console.log(res.status)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-  res.sendStatus(200);
+  res.send("bruder you made it lol");
 })
 
 app.listen(port, () => {
